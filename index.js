b0739e7da666c77e8ec47729cb0fed6b0731089b
SaiSanthoshKumarPokala <saisanthoshkumarpokala@gmail.com>
2026-04-21 22:43:58 +0530

    Modified the origin with frontend deployment link.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://kisan-connect-project.vercel.app/",
     // origin: ["http://192.168.1.7:5173","http://localhost:5173"],
     allowedHeaders: ["Authorization", "Content-Type"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
