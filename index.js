d127578a145e4c5f0cac02380fb0d9c2cbb12fcc
SaiSanthoshKumarPokala <saisanthoshkumarpokala@gmail.com>
2026-04-22 00:37:03 +0530

    Bug fixed in origin

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors({
-    origin: "https://kisan-connect-project.vercel.app/",
+    origin: ["https://kisan-connect-project.vercel.app","https://kisan-connect-frontend-ofo-saisantoshkumarpokala-7827s-projects.vercel.app"],
     // origin: ["http://192.168.1.7:5173","http://localhost:5173"],
     allowedHeaders: ["Authorization", "Content-Type"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
